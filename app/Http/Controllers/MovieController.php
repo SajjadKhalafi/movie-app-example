7f2d8f75275fba2226511c7e3e14de27891a74c3
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2023-01-26 13:38:31 +0330

    show popular movies & now playing movies from API

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $popularMovies = Http::get('https://api.themoviedb.org/3/movie/popular?api_key='.env('TMDB_TOKEN'))
+            ->json()['results'];
+
+        $nowPlaying = Http::get('https://api.themoviedb.org/3/movie/now_playing?api_key='.env('TMDB_TOKEN'))
+            ->json()['results'];
+
+        $nowPlayingDate = Http::get('https://api.themoviedb.org/3/movie/now_playing?api_key='.env('TMDB_TOKEN'))
+            ->json()['dates'];
+
+        $genresArray = Http::get('https://api.themoviedb.org/3/genre/movie/list?api_key='.env('TMDB_TOKEN'))
+            ->json()['genres'];
+
+        $genres = collect($genresArray)->mapWithKeys(function ($genre){
+            return [$genre['id'] => $genre['name']];
+        });
+
+        dump($nowPlayingDate);
+        return view('index' , compact('popularMovies','nowPlaying', 'genres'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'index');
-Route::view('/movie', 'show');
+Route::get('/', [MovieController::class , 'index'])->name('movies.index');
+Route::get('/movies/{movie}', [MovieController::class , 'show'])->name('movies.show');
+
