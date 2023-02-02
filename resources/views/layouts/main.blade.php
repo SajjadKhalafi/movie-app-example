14d76be00f0384f511e98b604b3a53414913ed75
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2023-02-02 22:46:44 +0330

    Add Dropdown to main navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,10 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie App</title>
     @vite('resources/css/app.css')
-    <livewire:styles />
+    <livewire:styles/>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 <body class="font-sans bg-gray-900 text-white">
+{{--<nav :class="{'flex': open, 'hidden': !open}" class="flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row">--}}
+{{--    <a class="px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-gray-200 rounded-lg dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Blog</a>--}}
+{{--    <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Portfolio</a>--}}
+{{--    <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">About</a>--}}
+{{--    <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Contact</a>--}}
+{{--    <div @click.away="open = false" class="relative" x-data="{ open: false }">--}}
+{{--        <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">--}}
+{{--            <span>Dropdown</span>--}}
+{{--            <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>--}}
+{{--        </button>--}}
+{{--        <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">--}}
+{{--            <div class="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">--}}
+{{--                <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #1</a>--}}
+{{--                <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #2</a>--}}
+{{--                <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #3</a>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</nav>--}}
 <nav class="border-b border-gray-800">
     <div class="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between px-4 py-6">
         <ul class="flex flex-col md:flex-row items-center">
@@ -22,17 +41,81 @@
                 </a>
             </li>
             <li class="md:ml-16 mt-3 md:mt-0">
-                <a href="{{ route('movies.index') }}" class="hover:text-gray-300">Movies</a>
+                <div class="relative" x-data="{ openMovies: false }">
+                    <button @click="openMovies = !openMovies"
+                            class="flex flex-row items-center">
+                        <span>Movies</span>
+                    </button>
+                    <div x-show="openMovies" @click.away="openMovies = false"
+                         x-transition:enter="transition ease-out duration-100"
+                         x-transition:enter-start="transform opacity-0 scale-95"
+                         x-transition:enter-end="transform opacity-100 scale-100"
+                         x-transition:leave="transition ease-in duration-75"
+                         x-transition:leave-start="transform opacity-100 scale-100"
+                         x-transition:leave-end="transform opacity-0 scale-95"
+                         class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48 z-50">
+                        <div class="px-2 py-2 bg-white rounded-md">
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold rounded-lg
+                             md:mt-0 text-gray-900 focus:text-gray-900 bg-gray-200 focus:bg-gray-200 focus:outline-none
+                             focus:shadow-outline"
+                               href="{{ route('movies.index') }}">Popular</a>
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold rounded-lg
+                             md:mt-0 text-gray-900 focus:text-gray-900 bg-gray-200 focus:bg-gray-200 focus:outline-none
+                             focus:shadow-outline"
+                               href="#">Now Playing</a>
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold rounded-lg
+                             md:mt-0 text-gray-900 focus:text-gray-900 bg-gray-200 focus:bg-gray-200 focus:outline-none
+                             focus:shadow-outline"
+                               href="#">Upcoming</a>
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold rounded-lg
+                             md:mt-0 text-gray-900 focus:text-gray-900 bg-gray-200 focus:bg-gray-200 focus:outline-none
+                             focus:shadow-outline"
+                               href="#">Top Rated</a>
+                        </div>
+                    </div>
+                </div>
             </li>
             <li class="md:ml-6 mt-3 md:mt-0">
-                <a href="{{ route('tv.index') }}" class="hover:text-gray-300">TV Shows</a>
+                <div class="relative" x-data="{ openTVShows: false }">
+                    <button @click="openTVShows = !openTVShows"
+                            class="flex flex-row items-center">
+                        <span>TV Shows</span>
+                    </button>
+                    <div x-show="openTVShows" @click.away="openTVShows = false"
+                         x-transition:enter="transition ease-out duration-100"
+                         x-transition:enter-start="transform opacity-0 scale-95"
+                         x-transition:enter-end="transform opacity-100 scale-100"
+                         x-transition:leave="transition ease-in duration-75"
+                         x-transition:leave-start="transform opacity-100 scale-100"
+                         x-transition:leave-end="transform opacity-0 scale-95"
+                         class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48 z-50">
+                        <div class="px-2 py-2 bg-white rounded-md">
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold rounded-lg
+                             md:mt-0 text-gray-900 focus:text-gray-900 bg-gray-200 focus:bg-gray-200 focus:outline-none
+                             focus:shadow-outline"
+                               href="{{ route('tv.index') }}">Popular</a>
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold rounded-lg
+                             md:mt-0 text-gray-900 focus:text-gray-900 bg-gray-200 focus:bg-gray-200 focus:outline-none
+                             focus:shadow-outline"
+                               href="#">Airing Today</a>
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold rounded-lg
+                             md:mt-0 text-gray-900 focus:text-gray-900 bg-gray-200 focus:bg-gray-200 focus:outline-none
+                             focus:shadow-outline"
+                               href="#">On TV</a>
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold rounded-lg
+                             md:mt-0 text-gray-900 focus:text-gray-900 bg-gray-200 focus:bg-gray-200 focus:outline-none
+                             focus:shadow-outline"
+                               href="#">Top Rated</a>
+                        </div>
+                    </div>
+                </div>
             </li>
             <li class="md:ml-6 mt-3 md:mt-0">
                 <a href="{{ route('actors.index') }}" class="hover:text-gray-300">Actors</a>
             </li>
         </ul>
         <div class="flex flex-col md:flex-row items-center">
-            <livewire:search-dropdown />
+            <livewire:search-dropdown/>
             <div class="md:ml-4 mt-3 md:mt-0">
                 <a href="#">
                     <img src="{{ asset('avatar.jpg') }}" alt="avatar" class="rounded-full w-8 h-8">
@@ -42,7 +125,7 @@
     </div>
 </nav>
 @yield('content')
-<livewire:scripts />
+<livewire:scripts/>
 @yield('scripts')
 </body>
 </html>
