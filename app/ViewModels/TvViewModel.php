1a4327fdfc8c802f285862a6a91f66a6993b5c3f
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2023-02-02 14:48:14 +0330

    Add TV Shows && show info

---
+++ app/ViewModels/TvViewModel.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\ViewModels;
+
+use Carbon\Carbon;
+use Spatie\ViewModels\ViewModel;
+
+class TvViewModel extends ViewModel
+{
+    public $popularTV;
+    public $topRated;
+    public $genres;
+
+    public function __construct($popularTV, $topRated, $genres)
+    {
+        $this->popularTV = $popularTV;
+        $this->topRated = $topRated;
+        $this->genres = $genres;
+    }
+
+    public function popularTv()
+    {
+        return $this->formatTv($this->popularTV);
+    }
+
+    public function topRated()
+    {
+        return $this->formatTv($this->topRated);
+    }
+
+    public function genres()
+    {
+        return collect($this->genres)->mapWithKeys(function ($genre) {
+            return [$genre['id'] => $genre['name']];
+        });
+    }
+
+    private function formatTv($tv)
+    {
+        return collect($tv)->map(function ($tvShow) {
+            $genresFormatted = collect($tvShow['genre_ids'])->mapWithKeys(function ($value) {
+                return [$value => $this->genres()->get($value)];
+            })->implode(', ');
+
+            return collect($tvShow)->merge([
+                'poster_path' => 'https://image.tmdb.org/t/p/w500/' . $tvShow['poster_path'],
+                'vote_average' => $tvShow['vote_average'] * 10 . '%',
+                'first_air_date' => Carbon::parse($tvShow['first_air_date'])->format('M d, Y'),
+                'genres' => $genresFormatted,
+            ])->only([
+                'poster_path', 'vote_average', 'first_air_date', 'genres', 'id', 'genre_ids', 'name',
+            ]);
+        });
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\ActorsController;
+use App\Http\Controllers\TvController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MovieController::class , 'index'])->name('movies.index');
 Route::get('/movies/{movie}', [MovieController::class , 'show'])->name('movies.show');
+
+Route::resource('/tv', TvController::class)->only(['index' , 'show']);
 
 Route::resource('/actors' , ActorsController::class)->only('index' , 'show');
 Route::get('/actors/page/{page?}', [ActorsController::class , 'index']);
