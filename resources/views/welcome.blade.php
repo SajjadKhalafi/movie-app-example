a663798c46d94bfb66cfdcc61e3eb2a911615756
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2023-02-06 20:10:11 +0330

    Add hero to landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,40 @@
 @extends('layouts.main')
 
 @section('content')
+
+    <section
+        style="background-image: linear-gradient(to right, rgba(3,37,65, 0.8) 0%, rgba(3,37,65, 0) 100%), url('https://www.themoviedb.org/t/p/w1920_and_h600_multi_faces_filter(duotone,00192f,00baff)/8bcoRX3hQRHufLPSDREdvr3YMXx.jpg');"
+        class="container mx-auto bg-cover">
+        <div class="max-w-screen-xl lg:py-16 lg:px-12">
+            <div class="w-full mb-[20px]">
+                <h2 class="text-[3rem] font-[700] text-white md:text-5xl lg:text-6xl">Welcome.</h2>
+                <p class="text-[2rem] font-[600] text-white">Millions of movies, TV shows and people to discover.
+                    Explore now.</p>
+            </div>
+
+            <form class="mt-[30px]">
+                <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                <div class="relative">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none"
+                             stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                        </svg>
+                    </div>
+                    <input type="search" id="default-search"
+                           class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           placeholder="Search for a movie, tv show, person......" required>
+                    <button type="submit"
+                            class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Search
+                    </button>
+                </div>
+            </form>
+
+        </div>
+    </section>
+
     <div class="container mx-auto px-4 pt-16">
         <div class="trending" x-data="{ isOpen: true, trends: {{ $dayTrends }} }">
             <div class="inline-flex rounded-md shadow-sm" role="group">
